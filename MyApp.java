9cd7378d35d7fa1ed9edfe29e6ff4cebb60d273f
SEONMIN99 <seonmin99@naver.com>
2020-04-09 02:30:21 +0900

    Update main & RobotWalk method

--- MyApp.java
+++ MyApp.java
@@ -1,34 +1,38 @@
-
+import java.util.Random;
 /**
  * 여기에 MyApp 클래스 설명을 작성하십시오.
  * 
  * @author (2018315040 김선민, 2017315038 이수정) 
  * @version (2020.04.07)
  */
-import java.util.Random;
+
 public class MyApp
 {
-//    public String signal;
-//    public double speed = 1;
-   
-    public double speed = 1;
-    public void RobotWalk(double speed)
+    public static void main(String[] args)
     {
         Random sRandom = new Random();
         String[] s = {"green", "yellow", "red"};
-        int signal = sRandom.nextInt(s.length);
-        if (signal == 0){
-            if(signal == 1){
+        String signal = s[sRandom.nextInt(s.length)];
+        System.out.println(signal);
+        System.out.println(RobotWalk(signal, 20));
+    }
+
+    public static double RobotWalk(String signal, double speed)
+    {
+        if (signal == "green")
+        {
+        }
+        else
+        {
+            if(signal == "yellow")
+            {
                 speed = speed * 1.2;
             }
-        }
-        else{
-            speed = 0;
+            else        
+            {
+                speed = 0;
+            }
         }
         return speed;
-    }
-     public static void main(String[] args)
-    {
-        System.out.print();
     }
 }
