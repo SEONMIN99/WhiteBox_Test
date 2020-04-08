26bb0adef6d56327d7bd5bc18380faa6c7823fb2
sujeong <tnwjd3730@naver.com>
2020-04-08 21:13:29 +0900

    test01(Random and Array)

--- MyApp.java
+++ MyApp.java
@@ -5,15 +5,30 @@
  * @author (2018315040 김선민, 2017315038 이수정) 
  * @version (2020.04.07)
  */
+import java.util.Random;
 public class MyApp
 {
-    public static void main(String[] args)
+//    public String signal;
+//    public double speed = 1;
+   
+    public double speed = 1;
+    public void RobotWalk(double speed)
     {
-    
+        Random sRandom = new Random();
+        String[] s = {"green", "yellow", "red"};
+        int signal = sRandom.nextInt(s.length);
+        if (signal == 0){
+            if(signal == 1){
+                speed = speed * 1.2;
+            }
+        }
+        else{
+            speed = 0;
+        }
+        return speed;
     }
-    
-    public static void RobotWalk()
+     public static void main(String[] args)
     {
-    
+        System.out.print();
     }
 }
