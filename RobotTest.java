189a9a42164473614b9439e141997a518bdb07f3
sujeong <tnwjd3730@naver.com>
2020-04-10 12:04:02 +0900

    RobotTest print Update

--- RobotTest.java
+++ RobotTest.java
@@ -10,8 +10,13 @@ public class RobotTest
 {
     public static void main(String[] args)
     {
-        System.out.println("신호:녹색, " + "속도:" + Robot.RobotWalk("녹색", 20));
-        System.out.println("신호:노란색," + "속도:" + Robot.RobotWalk("노란색", 20));
-        System.out.println("신호:빨간색, " + "속도:" + Robot.RobotWalk("빨간색", 20));
+        System.out.println("Statement Coverage test");
+        System.out.println("신호: 녹색, " + "속도:" + Robot.RobotWalk("녹색", 20));
+        System.out.println("신호: 노란색, " + "속도:" + Robot.RobotWalk("노란색", 20));
+        System.out.println("신호: 빨간색, " + "속도:" + Robot.RobotWalk("빨간색", 20));
+        
+        System.out.println("Decision Coverage test");
+        System.out.println("신호: 노란색, " + "속도:" + Robot.RobotWalk("노란색", 20));
+        System.out.println("신호: 빨간색, " + "속도:" + Robot.RobotWalk("빨간색", 20));
     }
 }
